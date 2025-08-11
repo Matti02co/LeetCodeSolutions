186ccfe9d9417e9f4a9b17965c0a468703057bea
Mattia Cocco <129308916+Matti02co@users.noreply.github.com>
2025-08-11 15:30:14 +0200

    Update 3-LongestSubstringWithoutRepeatingCharacters.py
    Optimized version

--- medium/3-LongestSubstringWithoutRepeatingCharacters.py
+++ medium/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -17,30 +17,30 @@ class SolutionA(object):
         check= 0    #index of repeated char
 
         for c in range(len(s)):
-            if s[c] not in present:    #if current char is not present, add +1 to current length and append char to list
+            if s[c] not in present:    #if current char is not present, adds +1 to current length and append char to list
                 present.append(s[c])
                 currentMax+=1
-            else:                         #else update maximum if appropriate
+            else:                         #else updates maximum if appropriate
                 if currentMax>totalMax:
                     totalMax=currentMax
                
                 x=len(present)-1
                 while x>=0:
                     if present[x]==s[c]:
-                        check=x               #find repeated char index
+                        check=x               #finds repeated char index
                         break
                     x-=1
 
-                if check==c-1:               #update currentMax
+                if check==c-1:               #updates currentMax
                     currentMax=0
                 else:
                     currentMax=c-(check+1)
 
                 y=0
-                present[:(check+1)]= [True] * (check+1)    #insert an inert value in list for every element before repeated char
+                present[:(check+1)]= [True] * (check+1)    #inserts an inert value in list for every element before repeated char
 
 
-                present.append(s[c])    #add element to list and update currentMax
+                present.append(s[c])    #adds element to list and update currentMax
                 currentMax+=1
         
         if currentMax>totalMax:
@@ -52,25 +52,21 @@ class SolutionA(object):
 """
 this solution works and uses a map to access O(1) the index of a char
 """
-class SolutionB(object):
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        if len(s) <= 1:
-            return len(s)
+        present={}    #maps char -> index where it was last seen
+        start=0    # start of window
+        maxLength=0    
 
-        present = {}    # map char-> last seen index
-        totalMax = 0    #max length
-        start = 0     # window start
-
-        for c in range(len(s)):
-            if s[c] in present and present[s[c]] >= start:
-                # update window start
-                start = present[s[c]] + 1
-            present[s[c]] = c                 #update index of char
-            totalMax = max(totalMax, c - start + 1)    #calculate max length until now
-
-        return totalMax
+        for i in range(len(s)):
+            if s[i] in present and present[s[i]]>=start:    #if char is in current window updates start
+                start= present[s[i]] +1
+            present[s[i]]=i                                 #inserts char in map 
+            maxLength=max(maxLength, i-start+1)             #updates maxLength
+        
+        return maxLength
 
